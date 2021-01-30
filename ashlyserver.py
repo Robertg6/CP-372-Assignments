1c0d57ef8b9205c183b32bb191709e881d4f2434
janelletait <65558755+janelletait@users.noreply.github.com>
2021-01-30 13:40:47 -0500

    Create ashlyserver.py

---
+++ ashlyserver.py
@@ -0,0 +1,42 @@
+'''
+Created on 2021 Jan 28
+
+@author: jtait
+'''
+from socket import *
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+#Prepare a sever socket
+serverSocket.bind((gethostname(), 8888))
+serverSocket.listen(1)
+
+while True:
+    #Establish the connection
+    print('\n\nReady to serve...')
+    connectionSocket, address = serverSocket.accept()
+    try:
+        # Receive HTTP request from browser
+        message = connectionSocket.recv(1024)
+        print("\n" + message)
+
+        # Open file mentioned in the first line of the HTTP request header
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+
+        #Send one HTTP header line into socket
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n")
+
+        #Send the content of the requested file to the client
+        connectionSocket.send(outputdata)
+        connectionSocket.close()
+
+    except IOError:
+        #Send response message for file not found
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n")
+
+        #Close client socket
+        connectionSocket.close()
+
+serverSocket.close()
